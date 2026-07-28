85d175aa6ebf477ae2aba946f06af741da4c51c1
Fevzi Berke URGANİOĞLU <fberkeurgani@gmail.com>
2026-07-28 15:17:11 +0300

    white screen

--- frontend/schema.sql
+++ frontend/schema.sql
@@ -14,4 +14,4 @@ CREATE TABLE partners (
 
 -- Test edebilmemiz için içine ilk verimizi ekliyoruz
 INSERT INTO partners (company_name, logo_url, website_url, sort_order, is_active) 
-VALUES ('Ciğerci Özkan', 'https://resmim.net/cdn/2026/07/28/EkTtJ6.png','https://www.cigerciozkan.com', 1, 1);
+VALUES ('Ciğerci Özkan','https://resmim.net/cdn/2026/07/28/EkTtJ6.png','https://www.cigerciozkan.com', 1, 1);
